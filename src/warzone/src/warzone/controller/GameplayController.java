40d4f8c2199dd4c5dc2f090080dc937d61d92e3d
parikhalay <parikh.alay58@gmail.com>
2023-09-19 02:44:40 -0400

    Show Map changes

--- src/warzone/src/warzone/controller/GameplayController.java
+++ src/warzone/src/warzone/controller/GameplayController.java
@@ -4,19 +4,20 @@ import warzone.view.*;
 import warzone.model.*;
 
 public class GameplayController {
-	
+
+	private GameContext d_gameContext;
+
+	public GameplayController(GameContext p_gameContext){
+		d_gameContext = p_gameContext;
+	}
 	/**
 	 * Performs the action for user command: showmap 
 	 * 
 	 * Shows all countries and continents, armies on each country, ownership, 
 	 * and connectivity in a way that enables efficient game play
-	 * 
-	 * @return
 	 */
-	public GameContext showMap() {
-		
-		// TODO Auto-generated method stub
-		
-		return null;
+	public void showMap() {
+		MapView.printMap(d_gameContext.getContinents());
+		MapView.printMapWithArmies(d_gameContext.getContinents());
 	}
 }
