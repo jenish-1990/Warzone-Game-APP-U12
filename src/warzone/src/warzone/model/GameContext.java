f7efa5e729f2886533b001f5a33bcd7c78df57b4
Shrutipavasiya <shrutipavasiya17012002@gmail.com>
2023-09-25 02:50:53 -0400

    map validation

--- src/warzone/src/warzone/model/GameContext.java
+++ src/warzone/src/warzone/model/GameContext.java
@@ -30,55 +30,6 @@ public class GameContext {
 		d_players = new HashMap<String, Player>() ;
 		d_countries = new HashMap<Integer, Country>();
 		d_continents = new HashMap<Integer, Continent>();
-//
-//		d_continents.put(1,new Continent(1,"North_America", "Red", 3 ));
-//		d_continents.put(2,new Continent(2,"South_America", "Blue", 5 ));
-//		d_continents.put(3,new Continent(2,"Asia", "Pink", 5 ));
-//
-//		Country canada = new Country(1,"Canada", 343,435 );
-//		d_countries.put(1,canada);
-//		d_continents.get(1).addCountry(canada);
-//
-//		Country us = new Country(2,"US", 343,435 );
-//		d_countries.put(2,us);
-//		d_continents.get(1).addCountry(us);
-//
-//		Country mexco = new Country(3,"mexco", 343,435 );
-//		d_countries.put(3,mexco);
-//		d_continents.get(1).addCountry(mexco);
-//
-//		Country cuba = new Country(4,"cuba", 343,435 );
-//		d_countries.put(4,cuba);
-//		d_continents.get(2).addCountry(cuba);
-//
-//		Country brazil = new Country(5,"brazil", 343,435 );
-//		d_countries.put(5,brazil);
-//		d_continents.get(2).addCountry(brazil);
-//
-//		Country chili = new Country(6,"chili", 343,435 );
-//		d_countries.put(6,chili);
-//		d_continents.get(2).addCountry(chili);
-//
-//		Country agentina = new Country(7,"agentina", 343,435 );
-//		d_countries.put(7,agentina);
-//		d_continents.get(2).addCountry(agentina);
-//
-//		canada.addNeighbor(us);
-//		us.addNeighbor(canada);
-//		us.addNeighbor(mexco);
-//
-//		cuba.addNeighbor(brazil);
-//		cuba.addNeighbor(chili);
-//		cuba.addNeighbor(agentina);
-//		brazil.addNeighbor(agentina);
-//		brazil.addNeighbor(chili);
-//		chili.addNeighbor(agentina);
-//		agentina.addNeighbor(chili);
-//
-//
-		
-		
-		
 	}		
 	
 	public static GameContext getGameContext() {
@@ -86,6 +37,14 @@ public class GameContext {
 			GAME_CONTEXT = new GameContext();
 		}
 		return GAME_CONTEXT;
+	}
+	
+	/**
+	 * clear the game context 
+	 * @return a new game context
+	 */
+	public static GameContext clear(){
+		return GAME_CONTEXT = new GameContext();
 	}
 	
 	public Map<String, Player> getPlayers() {
