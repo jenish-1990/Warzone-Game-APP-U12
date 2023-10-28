7234bec9ff326d3087eccb4b7c9a935695fef976
Ayushhpatel04 <aayushhpatel04@gmail.com>
2023-10-28 05:08:05 -0400

    1003-integration-java

---
+++ src/warzone/test/warzone/state/TestSuite.java
@@ -0,0 +1,17 @@
+package warzone.state;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Test all test class of package warzone.state
+ */
+//@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	GamePlayTest.class,
+	MapEditorTest.class,
+	PhaseTest.class
+	})
+public class TestSuite {
+
+}
