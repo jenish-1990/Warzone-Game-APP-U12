a51c19ba67077680d3af79a7c93bc19b1fb0b653
jenish-1990 <ajakhed123@gmail.com>
2023-10-30 05:13:46 -0400

    Read invalid map integration test

---
+++ src/warzone/test/warzone/state/StartupTest.java
@@ -0,0 +1,75 @@
+package warzone.state;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import warzone.model.ControllerName;
+import warzone.model.GameContext;
+import warzone.model.Router;
+import warzone.service.GameEngine;
+import warzone.service.StartupService;
+
+/** 
+ * Test class for startup phase
+ */
+public class StartupTest {
+
+	private GameContext d_gameContext;
+	private GameEngine d_gameEngine;
+	private Startup d_startupState;
+	
+	@Before
+	public void setup() {
+		
+		d_gameContext = GameContext.getGameContext();
+		d_gameEngine = GameEngine.getGameEngine(d_gameContext);
+		d_startupState = new Startup(d_gameEngine);
+		
+		d_gameEngine.setPhase(d_startupState);
+	}
+	
+	@Test
+	public void loadMapWrongFileNameIntegrationTest() {
+		
+		String l_mapName = "invalid_map_name.mapp";
+		
+		d_gameContext.setCurrentRouter(new Router(ControllerName.STARTUP, "loadmap", l_mapName));
+		
+		d_startupState.loadMap(l_mapName);
+		
+		assertTrue(d_gameContext.getContinents().size() == 0);
+		assertTrue(d_gameContext.getCountries().size() == 0);
+		
+	}
+	
+	@Test
+	public void loadMapCorrectFileNameIntegrationTest() {
+		
+		String l_mapName = "europe.map";
+		
+		d_gameContext.setCurrentRouter(new Router(ControllerName.STARTUP, "loadmap", l_mapName));
+		
+		d_startupState.loadMap(l_mapName);
+		
+		assertTrue(d_gameContext.getContinents().size() == 4);
+		assertTrue(d_gameContext.getCountries().size() == 24);
+		
+	}
+	
+	@Test
+	public void loadMapLowConnectivityIntegrationTest() {
+		
+		String l_mapName = "not-connected.map";
+		
+		d_gameContext.setCurrentRouter(new Router(ControllerName.STARTUP, "loadmap", l_mapName));
+		
+		boolean l_isConnected = new StartupService(d_gameContext).loadMap(l_mapName);
+		
+		assertTrue(l_isConnected == false);
+		assertTrue(d_gameContext.getContinents().size() == 2);
+		assertTrue(d_gameContext.getCountries().size() == 5);
+		
+	}
+}
