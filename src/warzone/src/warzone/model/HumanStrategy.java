42bc098c3a092c7113019e41d9277ccaa5e08f6d
jenish-1990 <ajakhed123@gmail.com>
2023-11-29 00:06:04 -0500

    implement the HumanStrategy

--- src/warzone/src/warzone/model/HumanStrategy.java
+++ src/warzone/src/warzone/model/HumanStrategy.java
@@ -1,5 +1,9 @@
 package warzone.model;
 import java.util.List;
+import java.util.Scanner;
+
+import warzone.service.GameEngine;
+import warzone.view.GenericView;
 
 
 /**
@@ -8,11 +12,18 @@ import java.util.List;
 public class HumanStrategy extends PlayerStrategy {	
 	
 	/**
+	 * scanner of the command
+	 */
+	private transient Scanner d_keyboard = new Scanner(System.in);
+	
+
+	/**
 	 * constructor of HumanStrategy
 	 * @param p_player PlayerStrategy
 	 */
 	HumanStrategy(Player p_player){
 		super(p_player); 
+
 	}
 	
 	/**
@@ -20,13 +31,37 @@ public class HumanStrategy extends PlayerStrategy {
 	 * @return
 	 */
 	public Order createOrder() {
-		Order l_order = null;
 		
-		//todo: implement it with real order according to the spec
-		Country l_country = this.d_player.getConqueredCountries().entrySet().iterator().next().getValue();
-		l_order = new DeployOrder(this.d_player, l_country, 1);
-		
-		
+		//1. issue order from interaction
+		if(d_keyboard == null)
+			d_keyboard = new Scanner(System.in);
+		String l_command = d_keyboard.nextLine().trim();
+		Order l_order;
+
+
+
+		if(l_command.equalsIgnoreCase("help")) {
+			d_gameEngine.getPhase().help();
+			return null;
+		}
+		else if( l_command.equalsIgnoreCase("showmap") ) {
+			d_gameEngine.getPhase().showMap();
+			return null;
+		}
+		//check if the issue order has finished
+		else if(l_command.equalsIgnoreCase("done")){
+			this.d_player.setHasFinisedIssueOrder(true);
+
+			GenericView.println(String.format("---------- Finish issuing order for player [%s] in this turn.", this.d_player.getName()));
+			return null;
+		}
+
+		l_order = this.d_player.conventOrder(l_command);
+		if (l_order == null) {
+			GenericView.printWarning("Incorrect command, please retry.");
+			d_gameContext.getLogEntryBuffer().logIssueOrder("Error", "failed to issued an order", l_command);
+		}
+				
 		return l_order;
 	}
 }
